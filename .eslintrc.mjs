88e8db85602fe6efb29913cb3d303a0a2430a65e
Fathismael93 <fathismael@gmail.com>
2025-02-04 23:08:11 -0800

    first commit

---
+++ .eslintrc.mjs
@@ -0,0 +1,24 @@
+const eslintrc = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'next/core-web-vitals',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+};
+
+export default eslintrc;
