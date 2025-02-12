9540d38db1d021f9f636522ada7750bb38d55a63
Fathismael93 <fathismael@gmail.com>
2025-02-12 11:20:42 -0800

    Deleted one try catch block in Delete presentation api

--- app/api/dashboard/presentation/[id]/delete/route.js
+++ app/api/dashboard/presentation/[id]/delete/route.js
@@ -12,51 +12,41 @@ export async function DELETE({ params }) {
     console.log('params: ');
     console.log(params);
 
-    try {
-      console.log('We are in the beginning of the second tryCatch block');
-      await deletePresentationSchema.validate({ id });
+    console.log('We are in the beginning of the second tryCatch block');
+    await deletePresentationSchema.validate({ id });
 
-      const query = {
-        // give the query a unique name
-        name: 'delete-presentation',
-        text: 'DELETE FROM presentations WHERE presentation_id=$1',
-        values: [id],
-      };
+    const query = {
+      // give the query a unique name
+      name: 'delete-presentation',
+      text: 'DELETE FROM presentations WHERE presentation_id=$1',
+      values: [id],
+    };
 
-      console.log('We have set the query');
+    console.log('We have set the query');
 
-      console.log('We are starting to delete from db');
+    console.log('We are starting to delete from db');
 
-      client
-        .query(query)
-        .then(() => console.log('Presentation deleted with success'))
-        .catch((e) => console.log(e));
+    client
+      .query(query)
+      .then(() => console.log('Presentation deleted with success'))
+      .catch((e) => console.log(e));
 
-      client.end(function (err) {
-        if (err) {
-          console.log(err);
-          throw err;
-        }
+    client.end(function (err) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
 
-        console.log('Client Database is stopped');
-      });
+      console.log('Client Database is stopped');
+    });
 
-      return NextResponse.json(
-        {
-          success: true,
-          message: 'Presentation deleted successfully',
-        },
-        { status: 200 },
-      );
-    } catch (error) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: error.inner[0].message,
-        },
-        { status: 400 },
-      );
-    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: 'Presentation deleted successfully',
+      },
+      { status: 200 },
+    );
   } catch (e) {
     return NextResponse.json(
       {
