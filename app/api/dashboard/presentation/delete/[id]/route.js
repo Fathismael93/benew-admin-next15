9179ca7336f08d8d7b9370c3f39109f6f203ea8d
Fathismael93 <fathismael@gmail.com>
2025-02-12 11:45:02 -0800

    Logging the params

--- app/api/dashboard/presentation/delete/[id]/route.js
+++ app/api/dashboard/presentation/delete/[id]/route.js
@@ -4,7 +4,7 @@ import { deletePresentationSchema } from '@/utils/schemas';
 
 export const dynamic = 'force-dynamic';
 
-export async function DELETE({ params }) {
+export async function DELETE(params) {
   console.log(params);
   try {
     const { id } = params;
