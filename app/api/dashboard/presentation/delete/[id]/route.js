fe37da40b457a7b0d77104b2a57210cf933bd678
Fathismael93 <fathismael@gmail.com>
2025-02-12 21:26:07 -0800

    fORGOT TO ADD CURLY BRACES TO params props

--- app/api/dashboard/presentation/delete/[id]/route.js
+++ app/api/dashboard/presentation/delete/[id]/route.js
@@ -4,7 +4,7 @@ import { deletePresentationSchema } from '@/utils/schemas';
 
 export const dynamic = 'force-dynamic';
 
-export async function DELETE(params) {
+export async function DELETE({ params }) {
   console.log(await params);
   try {
     const { id } = await params;
