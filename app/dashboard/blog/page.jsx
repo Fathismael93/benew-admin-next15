1af75aeecad9f453775994a66c95e51f5840ea4c
Fathismael93 <fathismael@gmail.com>
2025-02-15 06:17:41 -0800

    Created server component for fetching all articles

--- app/dashboard/blog/page.jsx
+++ app/dashboard/blog/page.jsx
@@ -7,11 +7,15 @@ async function getPosts() {
 
   await axios
     .get('https://benew-admin-next15.vercel.app/api/dashboard/blog')
-    .then((response) => console.log(response.data.articles))
+    .then((response) => {
+      console.log('response articles');
+      console.log(response.data);
+      articles = response.data.articles;
+    })
     .catch((error) => console.log(error));
 
-  // console.log('articles: ');
-  // console.log(articles);
+  console.log('articles: ');
+  console.log(articles);
 
   return articles;
 }
