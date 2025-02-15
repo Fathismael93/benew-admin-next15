79a8c0bd6b7d6ba5b502466dcd9a0ce70bb4305e
Fathismael93 <fathismael@gmail.com>
2025-02-15 02:25:47 -0800

    Added server component for getting a single article

--- app/dashboard/blog/[id]/view/page.jsx
+++ app/dashboard/blog/[id]/view/page.jsx
@@ -1,102 +1,31 @@
-'use client';
-
-import { React, useEffect, useState } from 'react';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import { CldImage } from 'next-cloudinary';
 import axios from 'axios';
-import parse from 'html-react-parser';
-import styles from '@/ui/styling/dashboard/blog/view-article/view.module.css';
 import { articleIDSchema } from '@/utils/schemas';
+// import SingleArticle from '@/ui/pages/blog/SingleArticle';
 
-async function View({ params }) {
-  const { id } = await params;
-  const [article, setArticle] = useState('');
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+async function getSinglePost() {
+  try {
+    await articleIDSchema.validate({ id });
 
-  useEffect(async () => {
-    async function getSinglePost() {
-      try {
-        await articleIDSchema.validate({ id });
-
-        await axios
-          .get(`/api/dashboard/blog/${id}/view`)
-          .then((response) => {
-            console.log('response');
-            console.log(response);
-            setArticle(response.data.data);
-          })
-          .catch(() => {
-            setErrorMessage('Article inexistant !');
-          });
-      } catch (error) {
-        setErrorMessage('Article inexistant !');
-      }
-    }
-
-    await getSinglePost(id);
-  }, [id]);
-
-  // eslint-disable-next-line camelcase
-  const deleteArticle = async (article_id) => {
     await axios
-      // eslint-disable-next-line camelcase
-      .delete(`/api/dashboard/blog/${article_id}/delete`)
-      .then((response) => setIsSuccess(response.data.success))
-      .catch((error) => console.log(error));
-  };
-
-  if (isSuccess) {
-    redirect('/dashboard/blog/');
+      .get(`/api/dashboard/blog/${id}/view`)
+      .then((response) => {
+        console.log('response');
+        console.log(response);
+      })
+      .catch(() => {
+        console.log('Article inexistant !');
+      });
+  } catch (error) {
+    console.log('Article inexistant !');
   }
-
-  return (
-    <section>
-      {article ? (
-        <div className={styles.postDetailContainer}>
-          <div className={styles.postDetailTop}>
-            <p className={styles.dateWritten}>
-              {article && <span>{`Publié le ${article.created}`}</span>}
-            </p>
-            <div className={styles.postDetailButtons}>
-              <Link href={`blog/${id}/edit`}>
-                <button
-                  type="button"
-                  className={`${styles.addButton} ${styles.edit}`}
-                >
-                  Edit
-                </button>
-              </Link>
-              <button
-                type="button"
-                className={`${styles.addButton} ${styles.delete}`}
-                onClick={() => deleteArticle(id)}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-          <h1>{article && article.article_title}</h1>
-          <div className={styles.postDetailImage}>
-            <CldImage
-              priority
-              src={article.article_image}
-              alt="Image illustration of the article"
-              width={750}
-              height={500}
-              style={{ width: '100%', height: 'auto' }}
-            />
-          </div>
-          <div className={styles.postDetailPara}>
-            {article && parse(article.article_text)}
-          </div>
-        </div>
-      ) : (
-        <h2 className="error-center">{errorMessage}</h2>
-      )}
-    </section>
-  );
 }
 
-export default View;
+async function ViewArticle({ params }) {
+  const { id } = await params;
+
+  await getSinglePost(id);
+
+  return <p>Test Réussi</p>;
+}
+
+export default ViewArticle;
---
+++ ui/pages/blog/SingleArticle.jsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { React, useState } from 'react';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { CldImage } from 'next-cloudinary';
+import parse from 'html-react-parser';
+import styles from '@/ui/styling/dashboard/blog/view-article/view.module.css';
+import { articleIDSchema } from '@/utils/schemas';
+
+const SingleArticle = () => {
+  const [article, setArticle] = useState('');
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // eslint-disable-next-line camelcase
+  const deleteArticle = async (article_id) => {
+    await axios
+      // eslint-disable-next-line camelcase
+      .delete(`/api/dashboard/blog/${article_id}/delete`)
+      .then((response) => setIsSuccess(response.data.success))
+      .catch((error) => console.log(error));
+  };
+
+  if (isSuccess) {
+    redirect('/dashboard/blog/');
+  }
+
+  return (
+    <section>
+      {article ? (
+        <div className={styles.postDetailContainer}>
+          <div className={styles.postDetailTop}>
+            <p className={styles.dateWritten}>
+              {article && <span>{`Publié le ${article.created}`}</span>}
+            </p>
+            <div className={styles.postDetailButtons}>
+              <Link href={`blog/${id}/edit`}>
+                <button
+                  type="button"
+                  className={`${styles.addButton} ${styles.edit}`}
+                >
+                  Edit
+                </button>
+              </Link>
+              <button
+                type="button"
+                className={`${styles.addButton} ${styles.delete}`}
+                onClick={() => deleteArticle(id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+          <h1>{article && article.article_title}</h1>
+          <div className={styles.postDetailImage}>
+            <CldImage
+              priority
+              src={article.article_image}
+              alt="Image illustration of the article"
+              width={750}
+              height={500}
+              style={{ width: '100%', height: 'auto' }}
+            />
+          </div>
+          <div className={styles.postDetailPara}>
+            {article && parse(article.article_text)}
+          </div>
+        </div>
+      ) : (
+        <h2 className="error-center">{errorMessage}</h2>
+      )}
+    </section>
+  );
+};
+
+export default SingleArticle;
