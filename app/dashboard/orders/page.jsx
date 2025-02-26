8216f77d9bf0c8a36697b87be90edcc515a08b35
Fathismael93 <fathismael@gmail.com>
2025-02-26 04:41:32 -0800

    Implemented get all orders api

--- app/dashboard/orders/page.jsx
+++ app/dashboard/orders/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import OrdersList from '@/ui/pages/orders/OrdersList';
 import axios from 'axios';
+import OrdersList from '@/ui/pages/orders/OrdersList';
 
 async function getOrders() {
   let orders;
@@ -8,6 +8,9 @@ async function getOrders() {
   await axios
     .get('https://benew-admin-next15.vercel.app/api/dashboard/orders')
     .then((response) => {
+      console.log('REESPONSE IN SERVER COMPONENT OrdersPage');
+      console.log(response);
+
       orders = response.data.orders;
     })
     .catch((error) => console.log(error));
