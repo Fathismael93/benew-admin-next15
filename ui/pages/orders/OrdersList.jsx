cf392a0206843fc588aaefde1b4baff4c6c1cbec
Fathismael93 <fathismael@gmail.com>
2025-02-26 07:49:57 -0800

    Implemented the OrdersList client component and the orders.module.css

--- ui/pages/orders/OrdersList.jsx
+++ ui/pages/orders/OrdersList.jsx
@@ -1,11 +1,54 @@
 'use client';
 
 import React from 'react';
+import styles from '@/ui/styling/dashboard/orders/orders.module.css';
+import Search from '@/ui/components/dashboard/search';
 
 const OrdersList = ({ data }) => {
-  console.log('Orders received from api');
-  console.log(data);
-  return <div>OrdersList</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for an order..." />
+      </div>
+      <div className={styles.bottom}>
+        {data.length > 0 ? (
+          <div className={styles.orderList}>
+            {data.map((order) => (
+              <div key={order.order_id} className={styles.orderItem}>
+                <div className={styles.orderDetails}>
+                  <h3 className={styles.orderTitle}>
+                    {order.application_name}
+                  </h3>
+                  <p className={styles.orderCategory}>
+                    {order.application_category}
+                  </p>
+                  <p className={styles.orderPrice}>
+                    Price: ${order.order_price}
+                  </p>
+                  <p className={styles.orderStatus}>
+                    Status:{' '}
+                    <span
+                      className={
+                        order.order_payment_status ? styles.paid : styles.unpaid
+                      }
+                    >
+                      {order.order_payment_status ? 'Paid' : 'Unpaid'}
+                    </span>
+                  </p>
+                  <p className={styles.orderDate}>
+                    Created on:{' '}
+                    {new Date(order.order_created).toLocaleDateString()}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className={styles.noOrders}>No orders available.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrdersList;
