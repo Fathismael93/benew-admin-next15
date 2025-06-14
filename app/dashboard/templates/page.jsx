ae50791fabb52db9511b29402120a99e6b54586c
Fathismael93 <fathismael@gmail.com>
2025-06-14 22:39:44 +0300

    Updated templates/page.jsx

--- app/dashboard/templates/page.jsx
+++ app/dashboard/templates/page.jsx
@@ -1,14 +1,15 @@
 import ListTemplates from '@/ui/pages/templates/ListTemplates';
+import axios from 'axios';
 
 async function getTemplates() {
   let templates = [];
 
-  // await axios
-  //   .get('https://benew-admin-next15.vercel.app/api/dashboard/templates')
-  //   .then((response) => {
-  //     templates = response.data.templates;
-  //   })
-  //   .catch((error) => console.log(error));
+  await axios
+    .get('https://benew-admin-next15.vercel.app/api/dashboard/templates')
+    .then((response) => {
+      templates = response.data.templates;
+    })
+    .catch((error) => console.log(error));
 
   return templates;
 }
