28ef7840b746db183925d7a8b2275e119e15c321
Fathismael93 <fathismael@gmail.com>
2025-06-28 13:53:18 +0300

    Updated Navbar component

--- ui/components/dashboard/navbar/index.jsx
+++ ui/components/dashboard/navbar/index.jsx
@@ -1,12 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import {
-  MdNotifications,
-  MdOutlineChat,
-  MdPublic,
-  MdSearch,
-} from 'react-icons/md';
+import { MdNotifications, MdOutlineChat, MdPublic } from 'react-icons/md';
 
 import styles from './navbar.module.css';
 
@@ -17,10 +12,6 @@ function Navbar() {
     <div className={styles.container}>
       <div className={styles.title}>{pathname.split('/').pop()}</div>
       <div className={styles.menu}>
-        <div className={styles.search}>
-          <MdSearch />
-          <input type="text" placeholder="Search..." className={styles.input} />
-        </div>
         <div className={styles.icons}>
           <MdOutlineChat size={20} />
           <MdNotifications size={20} />
